5a50980445b997e25be1fb4e2792ff5c019caa66
Daniel Jun Suguimoto <danielsuguimoto@gmail.com>
2020-03-13 22:32:42 -0300

    Stop command per services

--- app/Tasks/Stop.php
+++ app/Tasks/Stop.php
@@ -9,6 +9,9 @@ class Stop extends Task
     /** @var bool $purge */
     protected $purge = true;
 
+    /** @var string $services */
+    protected $services;
+
     public function run(...$args): int
     {
         $tasks = [
@@ -25,20 +28,49 @@ class Stop extends Task
         return $this;
     }
 
+    public function services(string $services): self
+     {
+         $this->services = $services;
+
+         return $this;
+     }
+
     public function destroyContainers(): int
     {
         return $this->runTask('Turning off fwd', function () {
-            $args[] = 'down';
+            $args[] = 'rm';
+            $args[] = '--stop';
+            $args[] = '--force';
 
             if ($this->purge) {
-                $args[] = '--volumes';
-                $args[] = '--remove-orphans';
+                $args[] = '-v';
             }
 
-            return $this->runCommandWithoutOutput(
+            if (! is_null($this->services)) {
+                $args[] = ($this->services ?: env('FWD_START_DEFAULT_SERVICES'));
+            }
+
+            $exitCode = $this->runCommandWithoutOutput(
                 DockerCompose::makeWithDefaultArgs(...$args),
                 false
             );
+
+            if ($exitCode) {
+                return 1;
+            }
+
+            if ($this->purge && is_null($this->services)) {
+                $exitCode = $this->runCommandWithoutOutput(
+                    DockerCompose::makeWithDefaultArgs('down', '--remove-orphans'),
+                    false
+                );
+
+                if ($exitCode) {
+                    return 1;
+                }
+            }
+
+            return 0;
         });
     }
 }
