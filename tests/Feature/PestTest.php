c115dbd1053cabc1e4d66b2d33c4d64abd5929c7
Daniel Jun Suguimoto <danielsuguimoto@gmail.com>
2020-05-31 16:44:40 -0300

    Fixing pest tests

--- tests/Feature/PestTest.php
+++ tests/Feature/PestTest.php
@@ -7,28 +7,28 @@ use Tests\TestCase;
 
 class PestTest extends TestCase
 {
-    public function pestTest()
+    public function testPest()
     {
         $this->artisan('pest')->assertExitCode(0);
 
         $this->asFwdUser()->assertDockerComposeExec('app php vendor/bin/pest');
     }
 
-    public function pestTestWithGroup()
+    public function testPestWithGroup()
     {
         $this->artisan('pest --group=something')->assertExitCode(0);
 
         $this->asFwdUser()->assertDockerComposeExec('app php vendor/bin/pest --group=something');
     }
 
-    public function pestTestWithCoverage()
+    public function testPestWithCoverage()
     {
         $this->artisan('pest --coverage')->assertExitCode(0);
 
         $this->asFwdUser()->assertDockerComposeExec('app php vendor/bin/pest --coverage');
     }
 
-    public function pestTestingWithDockerComposeFlags()
+    public function testPestingWithDockerComposeFlags()
     {
         app(Environment::class)->overloadEnv('tests/fixtures/.env.docker-compose_exec');
 
